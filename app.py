cacf89bc69727d27c0a9805dec8d67bb2ee3f564
suchimauz <alexey.fedulov98@gmail.com>
2021-05-13 17:03:36 +0300

    check

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import threading
 from aiogram import executor
 
 from handlers import dp
-from utils.misc.jobs import cancel_bank_accounts
+from utils.misc.jobs import cancel_bank_accounts, send_report_transactions
 from utils.notify_admins import on_startup_notify
 from utils.db.database import create_db
 
@@ -14,6 +14,7 @@ async def on_startup(dispatcher):
     await on_startup_notify(dispatcher)
     _thread = threading.Thread(target=asyncio.run, args=(cancel_bank_accounts(),))
     _thread.start()
+    await send_report_transactions()
 
     await create_db()
 
