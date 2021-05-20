54362a12d07ad28cf4d8e1d440c12f14ba5b5209
suchimauz <alexey.fedulov98@gmail.com>
2021-05-20 18:53:43 +0300

    fix

--- filters/is_admin.py
+++ filters/is_admin.py
@@ -14,7 +14,9 @@ class AdminFilter(BoundFilter):
 
     async def check(self, message: Union[types.Message, types.CallbackQuery]):
         is_admin = is_admin_func(message.from_user.id)
-        if not is_admin:
-            await message.answer("Вы не являетесь администратором!")
+
+        if self.is_admin is not None:
+            if not is_admin:
+                await message.answer("Вы не являетесь администратором!")
 
         return is_admin is self.is_admin
